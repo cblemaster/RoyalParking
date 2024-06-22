a27529f0b54d374cfadc186041188294a6b9c32b
Brian LeMaster <brian.lemaster@outlook.com>
2024-06-22 16:27:06 -0400

    added validator for register user dto to core project

---
+++ RoyalParking.Core/Validation/StringValidators.cs
@@ -0,0 +1,21 @@
+﻿using System.Text.RegularExpressions;
+
+namespace RoyalParking.Core.Validation;
+
+internal static class StringValidators
+{
+    internal static bool IsStringLengthValid(this string value, bool isRequired, uint maxLength)
+    {
+        if (!string.IsNullOrWhiteSpace(value))
+        {
+            uint minLength = isRequired ? (uint)1 : 0;
+            return value.Length >= minLength && value.Length <= maxLength;
+        }
+        return false;
+    }
+
+    internal static bool IsStringAllNumerals(this string value)
+    {
+        return value.ToCharArray().All(c => Regex.Match(c.ToString(), "[0-9]").Success);
+    }
+}
---
+++ RoyalParking.Core/Validation/ValidationResult.cs
@@ -0,0 +1,7 @@
+﻿namespace RoyalParking.Core.Validation;
+
+public class ValidationResult
+{
+    public required bool IsValid { get; init; }
+    public required string ErrorMessage { get; init; }
+}
