4eed9cec9cf117fdd3e5d221e4a0be1290d13904
Brian LeMaster <brian.lemaster@outlook.com>
2024-06-22 13:27:50 -0400

    moved db conn string from db context class and into config in api project; added jwt secret to config in api project; added to program.cs in api project: configuration of authentication/authorization, services for DI

--- RoyalParking.API/Program.cs
+++ RoyalParking.API/Program.cs
@@ -1,4 +1,51 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using RoyalParking.API.DataContext;
+using RoyalParking.Security;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
+
 var builder = WebApplication.CreateBuilder(args);
+
+IConfigurationRoot config = new ConfigurationBuilder()
+            .AddJsonFile("appsettings.json")
+            .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", optional: true)
+            .Build();
+
+string connectionString = config.GetConnectionString("Project") ?? "Error retrieving connection string!";
+string jwtSecret = config.GetValue<string>("JwtSecret") ?? "Error retreiving jwt config!";
+
+byte[] key = Encoding.ASCII.GetBytes(jwtSecret);
+JwtSecurityTokenHandler.DefaultInboundClaimTypeMap[JwtRegisteredClaimNames.Sub] = "sub";
+
+builder.Services.AddAuthentication(x =>
+    {
+        x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+        x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    })
+.AddJwtBearer(x =>
+    {
+        x.RequireHttpsMetadata = false;
+        x.SaveToken = true;
+        x.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey(key),
+            ValidateIssuer = false,
+            ValidateAudience = false,
+            NameClaimType = "name"
+        };
+    })
+.Services.AddAuthorizationBuilder()
+    .AddPolicy("customerpolicy", policy => policy.RequireRole("customer"))
+    .AddPolicy("valetpolicy", policy => policy.RequireRole("valet"))
+    .AddPolicy("allrolespolicy", policy => policy.RequireRole("customer", "valet"))
+.Services.AddDbContext<RoyalParkingContext>(options =>
+    options.UseSqlServer(connectionString))
+.AddSingleton<ITokenGenerator>(tk => new JwtGenerator(jwtSecret))
+.AddSingleton<IPasswordHasher>(ph => new PasswordHasher());
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
