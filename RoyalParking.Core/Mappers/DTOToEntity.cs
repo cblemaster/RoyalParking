3fb1d9756f24ee7f428a609e03e09511de2c452e
Brian LeMaster <brian.lemaster@outlook.com>
2024-06-22 15:37:56 -0400

    added dto to entity (and vice versa) mappers to core project

---
+++ RoyalParking.Core/Mappers/DTOToEntity.cs
@@ -0,0 +1,19 @@
+﻿using RoyalParking.Core.DTO;
+using RoyalParking.Core.Entities;
+
+namespace RoyalParking.Core.Mappers;
+
+public static class DTOToEntity
+{
+    public static User MapRegisterUserDTOToUser(RegisterUserDTO dto)
+    {
+        return new()
+        {
+            Username = dto.Username,
+            FirstName = dto.FirstName,
+            LastName = dto.LastName,
+            Email = dto.Email,
+            Phone = dto.Phone,
+        };
+    }
+}
---
+++ RoyalParking.Core/Mappers/EntityToDTO.cs
@@ -0,0 +1,24 @@
+﻿using RoyalParking.Core.DTO;
+using RoyalParking.Core.Entities;
+
+namespace RoyalParking.Core.Mappers;
+
+public static class EntityToDTO
+{
+    public static UserDTO MapUserToUserDTO(User entity)
+    {
+        return new()
+        {
+            Id = entity.Id,
+            Username = entity.Username,
+            FirstName = entity.FirstName,
+            LastName = entity.LastName,
+            Email = entity.Email,
+            Phone = entity.Phone,
+            CreateDate = entity.CreateDate,
+            UpdateDate = entity.UpdateDate,
+            CustomerId = entity.Customer?.Id,
+            ValetId = entity.Valet?.Id,
+        };
+    }
+}
