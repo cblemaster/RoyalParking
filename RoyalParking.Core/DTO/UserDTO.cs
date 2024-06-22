5c748dae4fec592a57eed0ec16709711e282b3dc
Brian LeMaster <brian.lemaster@outlook.com>
2024-06-22 14:25:11 -0400

    added dtos to core project

---
+++ RoyalParking.Core/DTO/UserDTO.cs
@@ -0,0 +1,17 @@
+﻿using System.Diagnostics.Contracts;
+
+namespace RoyalParking.Core.DTO;
+
+public class UserDTO
+{
+    public required int Id { get; init; }
+    public required string Username { get; init; }
+    public required string FirstName { get; init; }
+    public required string LastName { get; init; }
+    public required string Email { get; init; }
+    public required string Phone { get; init; }
+    public required DateTime CreateDate { get; init; }
+    public DateTime? UpdateDate { get; init; }
+    public int? CustomerId { get; init; }
+    public int? ValetId { get; init; }
+}
