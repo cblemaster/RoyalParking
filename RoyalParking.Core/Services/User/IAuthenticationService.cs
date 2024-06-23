da6264ef804155d31881a60f03ce39f5698cb170
Brian LeMaster <brian.lemaster@outlook.com>
2024-06-23 09:30:04 -0400

    updated interface in core project

--- RoyalParking.Core/Services/User/IAuthenticationService.cs
+++ RoyalParking.Core/Services/User/IAuthenticationService.cs
@@ -1,6 +1,14 @@
-﻿namespace RoyalParking.Core.Services.User;
+﻿using RoyalParking.Core.DTO;
+using RoyalParking.Core.Interfaces;
+
+namespace RoyalParking.Core.Services.User;
 
 public interface IAuthenticationService
 {
+    Task<IReturnable> GetUserById(int id);
+    Task<IEnumerable<IReturnable>> GetUsers();
+    Task<IReturnable> Register(RegisterUserDTO registerUser);
 
+    Task<IReturnable> LogIn(LoginUserDTO logInUser);
+    static void LogOut() => UserService.SetLogin(null!);
 }
