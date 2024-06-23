4ca4f007f56fb66cff41f58ef588bab9f9238432
Brian LeMaster <brian.lemaster@outlook.com>
2024-06-23 09:46:17 -0400

    added MAUI project (targeting windows desktop only) to solution

---
+++ RoyalParking.MAUI/MauiProgram.cs
@@ -0,0 +1,29 @@
+﻿using CommunityToolkit.Maui;
+using Microsoft.Extensions.Logging;
+using RoyalParking.Core.Services.User;
+
+namespace RoyalParking.MAUI;
+
+public static class MauiProgram
+{
+    public static MauiApp CreateMauiApp()
+    {
+        var builder = MauiApp.CreateBuilder();
+        builder
+            .UseMauiApp<App>()
+            .UseMauiCommunityToolkit()
+            .ConfigureFonts(fonts =>
+            {
+                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+            })
+            .Services.AddSingleton<AppShell>()
+                     .AddSingleton<IAuthenticationService, HttpAuthenticationService>();
+
+#if DEBUG
+		builder.Logging.AddDebug();
+#endif
+
+        return builder.Build();
+    }
+}
